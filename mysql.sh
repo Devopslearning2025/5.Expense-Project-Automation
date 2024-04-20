8757812b907f0e20241c6b2316f144ea56c2f52b
devops <dev0pslearning2025@gmail.com>
2024-04-20 22:45:39 +0530

    project-automation

---
+++ mysql.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SCRIPT_NAME=$(echo $0|cut -d '.' -f1)
+DATE=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with root user"
+else
+    echo "you are with root user"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo  -e "$2 is ... $R FAILURE $N"
+        #exit 1
+    else
+        echo -e "$2 is ... $G SUCCESS $N"
+    fi   
+}
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "installing my sql server"
