1d24df21649bc23c1b01e5dcad6c860a34c7a90f
devops <dev0pslearning2025@gmail.com>
2024-04-21 07:42:34 +0530

    Project

--- mysql.sh
+++ mysql.sh
@@ -40,7 +40,6 @@ systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "starting MySQL"
 
 #mysql -h 172.31.24.255 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
-#mysql -h 172.31.24.255 -uroot -p${mysql_password} -e 'show databases;' &>>$LOGFILE
 mysql -h db.devopslearning2025.online -uroot -p${mysql_password} -e 'show databases;' &>>$LOGFILE
 
 if [ $? -ne 0 ]
