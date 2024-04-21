e891a05d0aa9ee84e7520dfba7182a66968befd7
devops <dev0pslearning2025@gmail.com>
2024-04-21 07:34:57 +0530

    Project

--- mysql.sh
+++ mysql.sh
@@ -40,10 +40,10 @@ systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "starting MySQL"
 
 #mysql -h 172.31.24.255 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
-mysql -h 172.31.24.255 -uroot -p{mysql-password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.24.255 -uroot -p{mysql_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql-password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_password} &>>$LOGFILE
     VALIDATE $? "setting root password of MySQL"
 else
     echo -e "MySQL passowrd already set ....$Y SKIPPING  $N"
